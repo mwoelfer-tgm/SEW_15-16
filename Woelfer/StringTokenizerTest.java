d95dd64b01545c7720b2a7793704699ef2d1e0ce
Martin Wölfer <mwoelfer01.student@tgm.ac.at>
2016-04-11 15:15:18 +0200

    Initial commit

---
+++ Woelfer/StringTokenizerTest.java
@@ -0,0 +1,29 @@
+/*
+ * @Author Wölfer
+ * @Version 2016-04-11
+ */
+
+package Woelfer;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringTokenizerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
