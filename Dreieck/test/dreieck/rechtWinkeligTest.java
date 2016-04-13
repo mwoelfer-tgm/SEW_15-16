88ed3503cf7b2c460cb67a1868d4394342c55f1d
Martin Wölfer <mwoelfer01.student@tgm.ac.at>
2016-04-13 10:44:53 +0200

    added tests for rectangular triangle

--- Dreieck/test/dreieck/rechtWinkeligTest.java
+++ Dreieck/test/dreieck/rechtWinkeligTest.java
@@ -7,18 +7,60 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class rechtWinkeligTest {
-
+	int seite_a;
+	int seite_b;
+	int seite_c;
+	
+	private Dreieck dreieck;
+	
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		dreieck = new Dreieck(seite_a,seite_b,seite_c);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsNoTriangle() {
+		dreieck.setSeite_a(-1);
+		dreieck.setSeite_b(2);
+		dreieck.setSeite_c(3);
+		
+		assertFalse(dreieck.rechtWinkelig());
 	}
-
+	
+	@Test
+	public void testIstAGroesste() {
+		dreieck.setSeite_a(5);
+		dreieck.setSeite_b(4);
+		dreieck.setSeite_c(3);
+		
+		assertTrue(dreieck.rechtWinkelig());
+	}
+	
+	@Test
+	public void testIstBGroesste() {
+		dreieck.setSeite_a(4);
+		dreieck.setSeite_b(5);
+		dreieck.setSeite_c(3);
+		
+		assertTrue(dreieck.rechtWinkelig());
+	}
+	
+	@Test
+	public void testIstCGroesste() {
+		dreieck.setSeite_a(3);
+		dreieck.setSeite_b(4);
+		dreieck.setSeite_c(5);
+		
+		assertTrue(dreieck.rechtWinkelig());
+	}
+	
+	@Test
+	public void testIstGleichSeitig() {
+		dreieck.setSeite_a(5);
+		dreieck.setSeite_b(5);
+		dreieck.setSeite_c(5);
+		
+		assertFalse(dreieck.rechtWinkelig());
+	}
+	
 }
