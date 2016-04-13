40cd6ad1e8d87627e8a0565b63ef87729ded6511
Martin Wölfer <mwoelfer01.student@tgm.ac.at>
2016-04-13 09:32:26 +0200

    Initial commit

---
+++ Dreieck/test/dreieck/testIstDreieck.java
@@ -0,0 +1,85 @@
+package dreieck;
+
+/**
+ * 
+ * @author Martin Wölfer
+ * @email mwoelfer01@student.tgm.ac.at
+ * @version 2016-04-13
+ *
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testIstDreieck {
+	int seite_a;
+	int seite_b;
+	int seite_c;
+	
+	private Dreieck dreieck;
+
+	@Before
+	public void setUp() throws Exception {
+		dreieck = new Dreieck(seite_a,seite_b,seite_c);
+	}
+	
+	@Test
+	public void testIstDreickSeiteANull(){
+		dreieck.setSeite_a(0);
+		dreieck.setSeite_b(1);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void testIstDreickSeiteBNull(){
+		dreieck.setSeite_a(1);
+		dreieck.setSeite_b(0);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void testIstDreickSeiteCNull(){
+		dreieck.setSeite_a(1);
+		dreieck.setSeite_b(2);
+		dreieck.setSeite_c(0);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void testIstDreickSeiteAMinus(){
+		dreieck.setSeite_a(-1);
+		dreieck.setSeite_b(1);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void testIstDreickSeiteBMinus(){
+		dreieck.setSeite_a(1);
+		dreieck.setSeite_b(-1);
+		dreieck.setSeite_c(2);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	
+	@Test
+	public void testIstDreickSeiteCMinus(){
+		dreieck.setSeite_a(1);
+		dreieck.setSeite_b(2);
+		dreieck.setSeite_c(-1);
+		
+		assertFalse(dreieck.istDreieck());
+	}
+	@After
+	public void tearDown() throws Exception {
+	}
+}
